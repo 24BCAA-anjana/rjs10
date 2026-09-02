47e11daf8d559c033208043e6569e5385f4e714d
24BCAA-anjana <anjanababu250@gmail.com>
2026-09-02 10:13:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const {useState} = React;
+const { useState } = React;
 
-function Counter(){
-  const[count,setCount]=useState(0);
+function Counter() {
+    const [count, setCount] = useState(0);
 
-return{
-  <div>
-  <h2>You clicked the button{count} of times</h2>
-  <button onClick={()=>setCount(count+1)}>
-Click ME!!
-  </button>
-  </div>
-};
+    return (
+        <div>
+            <h2>You clicked the button {count} of times</h2>
+            <button onClick={() => setCount(count + 1)}>
+                Click ME!!!
+            </button>
+        </div>
+    );
 }
-const root=reactDOM createRoot(
-  document.getelementById("root")
-  };
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+
 root.render(
-  <counter/>
-  );
+    <Counter />
+);
